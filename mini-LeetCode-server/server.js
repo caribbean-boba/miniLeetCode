cbc5f639a0238ffd99f9ab886716c2ac01fb7346
yhl <lyuyanhan97@gmail.com>
2018-07-17 22:51:12 -0700

    backend also works

--- mini-LeetCode-server/server.js
+++ mini-LeetCode-server/server.js
@@ -4,6 +4,11 @@ var restApiRouter = require('./routes/rest-api');
 var mongoose = require("mongoose");
 var indexRouter = require("./routes/index");
 var path = require("path");
+var http = require('http');
+
+var socket_io = require('socket.io');
+var io = socket_io();
+var editorSocketService = require('./services/societ-io-service')(io);
 
 mongoose.connect('mongodb://yanhan:lyh19970409@ds249415.mlab.com:49415/minileetcode', { useNewUrlParser: true });
 
@@ -14,4 +19,22 @@ app.use('/',function(req, res){
     res.sendFile("index.html", {root: path.join(__dirname, '../public')});
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+// app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+
+var server = http.createServer(app);
+io.attach(server);
+server.listen(3000);
+
+server.on('error', onError);
+function onError(error){
+    throw error;
+}
+server.on('listening', onListening);
+function onListening(error){
+    var addr = server.address();
+    var bind = typeof addr == 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+    console.log('Listening on' + bind);
+}
